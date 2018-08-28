51b84d749e5272b7879fa0244e1855f066da0668
cory bonneau <cory.bonneau2424@gmail.com>
2018-08-28 22:34:21 +0200

    custome awesomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@ import "bootstrap";
 import "awesomplete/awesomplete.css"
 // Awesomplete JS
 import "awesomplete"
+import {awsompleteSettings} from "../components/awesomplete_settings.js"
+awsompleteSettings()
 
 import {carousel} from "../components/carousel.js";
 carousel()
