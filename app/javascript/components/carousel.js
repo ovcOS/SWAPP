78fad36a000878a526ea068f8bbadf2a527b4ed7
Tom Patton <tom.patton22@gmail.com>
2018-08-28 18:17:51 +0200

    landing page

---
+++ app/javascript/components/carousel.js
@@ -0,0 +1,12 @@
+function carousel() {
+  $(document).ready(function() {
+    //carousel options
+    $('#quote-carousel').carousel({
+      pause: true, interval: 10000,
+    });
+  });
+}
+
+export{carousel};
+/* Rectangle 3: */
+opacity: 0.74; background-image: linear-gradient(-134deg, #3023AE 0%, #C86DD7 100%);
