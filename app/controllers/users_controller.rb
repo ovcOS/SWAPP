81a8d4fa477ec7de9e2aa4c935edc8a90956580f
Oriol VidalCortes <ovidalcortes@gmail.com>
2018-08-30 18:42:27 +0200

    fix search bar, defaults, fonts, marketplace

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
   def index
     if params[:query].present?
       @users = User.search_by_fuzzy_skill(params[:query])
+      if @users.empty?
+        @users = User.best_matches(current_user)
+      end
     else
       @users = User.best_matches(current_user)
     end
