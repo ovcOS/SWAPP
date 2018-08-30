ba458733e68b481ff434880f3073c54f28030809
cory bonneau <cory.bonneau2424@gmail.com>
2018-08-30 20:20:13 +0200

    routes done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    redirect_to users_path if current_user
   end
 
   def profile
