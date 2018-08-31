b5f83ef578d7127728ad0c5c7ac86ae1495cc8eb
Nils Lundgren <n.lunpat@gmail.com>
2018-08-31 15:10:14 +0200

    commit fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,27 +19,42 @@ class UsersController < ApplicationController
 
   def update
     @user = current_user
-    @user.update_attributes(user_params)
+
+    @user.description = params[:user][:description] if user_params[:description]
+    if user_params[:skills]
+      skill = Skill.find_by_name(user_params[:skills].downcase.gsub(' ', '-'))
+      @user.skills << skill if skill
+    end
+
+    if user_params[:tags]
+      tag = Tag.find_by_name(user_params[:tags].downcase.gsub(' ', '_'))
+      @user.tags << tag if tag
+    end
+
+    if user_params[:profile_photo]
+      @user.profile_photo = params[:user][:profile_photo] if user_params[:profile_photo]
+    end
+
+    if params[:skills].present?
+      params[:skills].each do |skill_name|
+        @user.skills << Skill.find_by_name(skill_name)
+      end
+    end
+
+    if params[:tags].present?
+      params[:tags].each do |tag_name|
+        @user.tags << Tag.find_by_name(tag_name)
+      end
+    end
+
+    @user.save!
     redirect_to profile_path
-    #params[:user][:description]
-
-    # if params[:skills].present?
-    #   params[:skills].each do |skill_name|
-    #     @user.skills << Skill.find_by_name(skill_name)
-    #   end
-    # end
-
-    # if params[:tags].present?
-    #   params[:tags].each do |tag_name|
-    #     @user.tags << Tag.find_by_name(tag_name)
-    #   end
-    # end
   end
 
   def getting_started
     @user = current_user
-    @skill_names = Skill.all.map{ |skill| skill.name.gsub('_', ' ').titlecase }
-    @tag_names = Tag.all.map{ |tag| tag.name.gsub('_', ' ').titlecase }
+    @skill_names = Skill.all.map{ |skill| skill.name.titleize }
+    @tag_names = Tag.all.map{ |tag| tag.name.titleize }
   end
 
   private
@@ -49,6 +64,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :profile_photo, :description)
+    params.require(:user).permit(:name, :email, :profile_photo, :description, :skills, :tags)
   end
 end
