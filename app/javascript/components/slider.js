58b59fe818500f6ee8b4958fa0663823e8739968
Nils Lundgren <n.lunpat@gmail.com>
2018-09-02 04:25:33 +0200

    finished new home

--- app/javascript/components/slider.js
+++ app/javascript/components/slider.js
@@ -1,9 +1,13 @@
+//jquery-stealer
 function slider() {
   var carousel = document.querySelector('.carousel');
   if (carousel) {
     var hammer = new Hammer(carousel);
     hammer.on('swipeleft', function(e) {
       $('.carousel').carousel('next');
+    });
+    hammer.on('swiperight', function(e) {
+      $('.carousel').carousel('prev');
     });
   }
 }
