61465661975cc6ef9d8b0e4195e3e0c41cc5b2bc
Nils Lundgren <n.lunpat@gmail.com>
2018-09-02 23:48:40 +0200

    saved

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -3,3 +3,4 @@ class Connection < ApplicationRecord
   belongs_to :requester, class_name: 'User', foreign_key: :user_id
   belongs_to :responder, class_name: 'User', foreign_key: :responder_id
 end
+
