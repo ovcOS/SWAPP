364842d5f80f31f10df2587bd35dcb0482ed9625
Nils Lundgren <n.lunpat@gmail.com>
2018-09-04 23:51:41 +0200

    fixed webpack?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,11 @@ import "bootstrap";
 // Awesomplete CSS
 import "awesomplete/awesomplete.css";
 // Awesomplete JS
-
-import "awesomplete"
-
-import {awsompleteSettings} from "../components/awesomplete_settings.js"
-
 import "awesomplete";
+
 import {awsompleteSettings} from "../components/awesomplete_settings.js";
 awsompleteSettings();
+
 import {carousel} from "../components/carousel.js";
 carousel();
 import { switchTabs } from "../components/profile_tabs.js";
